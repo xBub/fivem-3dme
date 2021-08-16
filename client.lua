1196c47bd6d9db6282f71b9ad456e3981ab4ac2b
xBub_ <75557986+xBub@users.noreply.github.com>
2021-08-16 03:26:57 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -90,9 +90,9 @@ AddEventHandler('xBub_:sendProximityMessageMe', function(id, name, message)
   local myId = PlayerId()
   local pid = GetPlayerFromServerId(id)
   if pid == myId then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^6" .. name .. ":^7 " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^3" .. name .. ":^7 " .. message)
   elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 19.999 then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^6" .. name .. ":^7 " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^3" .. name .. ":^7 " .. message)
   end
 end)
 
