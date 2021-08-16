98ec4d581dfaad66cf277808c89166a05cc00e6b
xBub_ <75557986+xBub@users.noreply.github.com>
2021-08-16 08:24:06 -0400

    Add files via upload

--- client.lua
+++ client.lua
@@ -58,7 +58,7 @@ local function Display(ped, text)
         end)
 
         -- Display
-        local offset = 0.8 + pedDisplaying[ped] * 0.1
+        local offset = 0.5 + pedDisplaying[ped] * 0.1
         while display do
             if HasEntityClearLosToEntity(playerPed, ped, 17 ) then
                 local x, y, z = table.unpack(GetEntityCoords(ped))
@@ -89,10 +89,11 @@ RegisterNetEvent('xBub_:sendProximityMessageMe')
 AddEventHandler('xBub_:sendProximityMessageMe', function(id, name, message)
   local myId = PlayerId()
   local pid = GetPlayerFromServerId(id)
+  local plid = GetPlayerServerId()
   if pid == myId then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^3" .. name .. ":^7 " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^3" .. name .. "^6(#" .. id .. "):^7 " .. message)
   elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 19.999 then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^3" .. name .. ":^7 " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, "^5[ME] ^3" .. name .. "^6(#" .. id .. "):^7 " .. message)
   end
 end)
 
