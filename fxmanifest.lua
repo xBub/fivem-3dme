f9aa5787d0b50340f48e7c35a2b2dc42d81179b9
xBub_ <75557986+xBub@users.noreply.github.com>
2021-08-16 04:29:24 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'bodacious'
 games { 'gta5' }
 
+author 'xBub_#0001'
+description '/me FiveM command with chat and 3d print.'
+version '1.0.0'
+
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
